382297a406542d6a6841153f4a68c05376675e69
razzorsec <54918791+razzorsec@users.noreply.github.com>
2021-03-11 22:27:18 +0530

    Update Shor.py

--- Shor.py
+++ Shor.py
@@ -4,5 +4,5 @@ import numpy as np
 from qiskit import QuantumCircuit, Aer, execute
 backend = Aer.get_backend('qasm_simulator')
 quanInst =  QuantumInstance(backend, shots=1000)
-shorShot = Shor(N=35,a=3,quantum_instance=quanInst)
+shorShot = Shor(N=15,a=2,quantum_instance=quanInst)
 print(Shor.run(shorShot))
